a7bebbbb0f4441f51ad3d569f2c81bd5430fce42
willem_koonings@hotmail.com <willem_koonings@hotmail.com>
2019-10-03 21:12:14 -0500

    first commit

---
+++ crm/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+
+app_name = 'crm'
+urlpatterns = [
+    path('', views.home, name='home'),
+    re_path(r'^home/$', views.home, name='home'),
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    path('service_list', views.service_list, name='service_list'),
+    path('service/create/', views.service_new, name='service_new'),
+    path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
+    path('service/<int:pk>/delete/', views.service_delete, name='service_delete'),
+    path('product_list', views.product_list, name='product_list'),
+    path('product/create/', views.product_new, name='product_new'),
+    path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
+    path('customer/<int:pk>/summary/', views.summary, name='summary'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('password_change_form', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+]
